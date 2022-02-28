bb42d19017aa8a054b58fd87dda116897c7a19f4
ang <ag888927@gmail.com>
2022-02-28 16:44:13 -0600

    Finalizado union

--- app/src/main/java/com/application/pm1_proyecto_final/activities/MainActivity.java
+++ app/src/main/java/com/application/pm1_proyecto_final/activities/MainActivity.java
@@ -78,9 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                login();
-            }
+            public void onClick(View view) { login(); }
         });
 
         btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
