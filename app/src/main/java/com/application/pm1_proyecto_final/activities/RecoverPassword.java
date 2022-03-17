a71ece1c0bdc32db1ac8ddea0f58e59a098061d7
BrayanUTH <10brayanalvarez@gmail.com>
2022-03-17 17:00:38 -0600

    Actualizar password desde login finalizado correctamente

--- app/src/main/java/com/application/pm1_proyecto_final/activities/RecoverPassword.java
+++ app/src/main/java/com/application/pm1_proyecto_final/activities/RecoverPassword.java
@@ -33,13 +33,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecoverPassword extends AppCompatActivity {
 
     CircleImageView btnBackEditPassB;
     Button btnSendNewPassword;
-    TextView btnSendPassword, textInformation;
+    TextView btnSendAgainPassword, textInformation;
 
     EditText txtEmail;
     TextInputEditText txtNewPassword, txtPasswordTemp, txtPasswordConfirmRecover;
@@ -56,7 +58,7 @@ public class RecoverPassword extends AppCompatActivity {
 
         btnSendNewPassword = (Button) findViewById(R.id.btnSendNewPassword);
         btnBackEditPassB = (CircleImageView) findViewById(R.id.btnBackEditPassB);
-        btnSendPassword = (TextView) findViewById(R.id.btnSendCodeRecoverPassword);
+        btnSendAgainPassword = (TextView) findViewById(R.id.btnSendCodeRecoverPassword);
         textInformation = (TextView) findViewById(R.id.textInformationRecover);
 
         txtEmail = (EditText) findViewById(R.id.txtEmailRecoverPassword);
@@ -83,11 +85,11 @@ public class RecoverPassword extends AppCompatActivity {
             }
         });
 
-        btnSendPassword.setOnClickListener(new View.OnClickListener() {
+        btnSendAgainPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder1 = new AlertDialog.Builder(RecoverPassword.this);
-                builder1.setMessage("¿Enviar la contraseña nuevamente?");
+                builder1.setMessage("¿Generar la contraseña nuevamente?");
                 builder1.setCancelable(true);
 
                 builder1.setPositiveButton(
@@ -109,8 +111,8 @@ public class RecoverPassword extends AppCompatActivity {
                             }
                         });
 
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+                AlertDialog alert = builder1.create();
+                alert.show();
             }
         });
 
@@ -120,6 +122,12 @@ public class RecoverPassword extends AppCompatActivity {
 
         if (!visibilityFields) {
             user.setEmail(txtEmail.getText().toString().trim());
+
+            if (user.getEmail().isEmpty()) {
+                ResourceUtil.showAlert("Advertencia", "Debes ingresar el correo electrónico.", RecoverPassword.this, "error");
+                return;
+            }
+
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, UserApiMethods.EXIST_EMAIL + user.getEmail(), null, new Response.Listener<JSONObject>() {
                 @Override
@@ -149,10 +157,55 @@ public class RecoverPassword extends AppCompatActivity {
             });
             requestQueue.add(request);
         } else {
-            Toast.makeText(this, "Verificando y Guardando", Toast.LENGTH_SHORT).show();
+            String passwordTempUser = txtPasswordTemp.getText().toString();
+            String newPasswordUser = txtNewPassword.getText().toString();
+            String passwordConfirmUser = txtPasswordConfirmRecover.getText().toString();
+            String response = validateInputsPassword(passwordTempUser,newPasswordUser, passwordConfirmUser);
+
+            if (response.equals("OK")) {
+                updatePassword();
+            } else {
+                ResourceUtil.showAlert("Advertencia", response, RecoverPassword.this, "error");
+            }
+
         }
 
+    }
 
+    private void updatePassword() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("idFirebase", user.getIdFirebase());
+        params.put("name", user.getName());
+        params.put("lastname", user.getLastname());
+        params.put("numberAccount", user.getNumberAccount());
+        params.put("phone", user.getPhone());
+        params.put("status", user.getStatus());
+        params.put("address", user.getAddress());
+        params.put("birthDate", user.getBirthDate());
+        params.put("carrera", user.getCarrera());
+        params.put("image", user.getImage());
+        params.put("imageCover", user.getImageCover());
+        params.put("email", user.getEmail());
+        params.put("password", user.getPassword());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, UserApiMethods.PUT_USER+user.getId(), new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                setVisibilityFields("hide");
+                txtEmail.setText("");
+                passwordTemp = "";
+                visibilityFields = false;
+                ResourceUtil.showAlert("Confirmación", "Contraseña actualizada.",RecoverPassword.this, "success");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ResourceUtil.showAlert("Advertencia", "Se produjo un error al actualizar el password del usuario.",RecoverPassword.this, "error");
+                Log.d("ERROR_USER", "Error Update: "+error.getMessage());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 
     private void loadDataUser(JSONObject dataUser) {
@@ -180,34 +233,27 @@ public class RecoverPassword extends AppCompatActivity {
         }
     }
 
-    private void prepareUpdatePassword() {
-
-        if (!visibilityFields) {
-            // ENVIAR NUEVO PASSWORD
-
-
-        } else {
-
-        }
-
-    }
-
     private void setVisibilityFields(String action) {
         if (action.equalsIgnoreCase("view")) {
-            textInformation.setText("Le enviaremos su nueva contraseña al correo electrónico proporcionado.");
+            textInformation.setText("La contraseña enviada es temporal, por su seguridad ingrese su nueva contraseña y confirmela.");
             btnSendNewPassword.setText("SALVAR");
             containerPasswordTemp.setVisibility(View.VISIBLE);
             containerNewPassword.setVisibility(View.VISIBLE);
             containerPasswordConfirm.setVisibility(View.VISIBLE);
             txtEmail.setVisibility(View.GONE);
+            btnSendAgainPassword.setVisibility(View.VISIBLE);
         } else {
-            textInformation.setText("La contraseña enviada es temporal, por su seguridad ingrese su nueva contraseña y confirmela.");
+            textInformation.setText("Le enviaremos su nueva contraseña al correo electrónico proporcionado.");
             btnSendNewPassword.setText("OBTENER NUEVA CONTRASEÑA");
             containerPasswordTemp.setVisibility(View.GONE);
             containerNewPassword.setVisibility(View.GONE);
             containerPasswordConfirm.setVisibility(View.GONE);
             txtEmail.setVisibility(View.VISIBLE);
+            btnSendAgainPassword.setVisibility(View.GONE);
         }
+        txtNewPassword.setText("");
+        txtPasswordTemp.setText("");
+        txtPasswordConfirmRecover.setText("");
     }
 
     private void sendEmail(String email, String nameUser) {
@@ -218,6 +264,25 @@ public class RecoverPassword extends AppCompatActivity {
 
         JavaMailAPI javaMailAPI = new JavaMailAPI(this, email, subject, message);
         javaMailAPI.execute();
+    }
+
+    private String validateInputsPassword(String passwordTempUser, String newPassword, String passwordConfirm) {
+        String response = "OK";
+
+        if (passwordTempUser.isEmpty()) {
+            response = "Debe ingresar la contraseña enviada.";
+        } else if(newPassword.isEmpty()) {
+            response = "Debe ingresar la nueva contraseña.";
+        } else if(passwordConfirm.isEmpty()) {
+            response = "Debe ingresar la contraseña de confirmación.";
+        } else if(!passwordTempUser.equals(passwordTemp)) {
+            response = "La contraseña enviada ingresada no es correcta.";
+        }else if(!newPassword.equals(passwordConfirm)) {
+            response = "La nueva contraseña no coincide con la contraseña de confirmación.";
+        }
+
+        user.setPassword(newPassword);
+        return response;
     }
 
 }
