53912f30fa1e5c1a65a9fe03fb21200be586a8c7
Smolder98 <alicoescobar@gmail.com>
2022-03-24 22:50:38 -0600

    Agregando el editar grupo

--- app/src/main/java/com/application/pm1_proyecto_final/activities/InfoGroupActivity.java
+++ app/src/main/java/com/application/pm1_proyecto_final/activities/InfoGroupActivity.java
@@ -1,5 +1,7 @@
 package com.application.pm1_proyecto_final.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -11,6 +13,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.util.Patterns;
@@ -44,6 +47,7 @@ import com.application.pm1_proyecto_final.utils.PreferencesManager;
 import com.application.pm1_proyecto_final.utils.ResourceUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -54,10 +58,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class InfoGroupActivity extends AppCompatActivity {
 
@@ -65,7 +74,7 @@ public class InfoGroupActivity extends AppCompatActivity {
 
     TextView textViewDescription, textViewTitle, textViewImage;
 
-    AppCompatImageView imageViewBack;
+    AppCompatImageView imageViewBack, imageViewEdit;
 
     RoundedImageView roundedImageView;
 
@@ -75,7 +84,16 @@ public class InfoGroupActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewUsuarios;
 
-    Button btnAddMember;
+    Button btnAddMember, btnDeleteGroup, btnLeaveGroup;
+
+    String encodedImage = "";
+
+    SweetAlertDialog pDialog;
+
+    RoundedImageView roundedImageViewDialog;
+    Button btnSaveGroupDialog;
+    TextView textImageDialog;
+    TextInputEditText textInputEditTextTitle, textInputEditTextDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,27 +108,35 @@ public class InfoGroupActivity extends AppCompatActivity {
         loadUsersGroups();
     }
 
+
     private void init(){
         reseiverGroup = null;
 
         preferencesManager = new PreferencesManager(getApplicationContext());
 
+        pDialog = ResourceUtil.showAlertLoading(InfoGroupActivity.this);
+
         textViewTitle = (TextView) findViewById(R.id.textTitleGroupInfo);
         textViewDescription = (TextView) findViewById(R.id.textViewDescInfo);
         textViewImage = (TextView) findViewById(R.id.textImageInfo);
         imageViewBack = (AppCompatImageView) findViewById(R.id.btnInfoGroupBack);
+        imageViewEdit = (AppCompatImageView) findViewById(R.id.imageEditGroup);
 
         roundedImageView = (RoundedImageView) findViewById(R.id.imageGroupInfo);
 
         recyclerViewUsuarios = (RecyclerView) findViewById(R.id.usersGroupRecyclerView);
 
         btnAddMember = (Button) findViewById(R.id.btnAddMember);
+        btnDeleteGroup = (Button) findViewById(R.id.btnCerrarGrupo);
+        btnLeaveGroup = (Button) findViewById(R.id.btnSalirGrupo);
     }
 
     private void setListeners(){
         imageViewBack.setOnClickListener(v->onBackPressed());
 
         btnAddMember.setOnClickListener(v -> dialogAddMember());
+
+        imageViewEdit.setOnClickListener(v -> dialogEditGroup());
     }
 
     private void loadReceiverDetails(){
@@ -123,6 +149,18 @@ public class InfoGroupActivity extends AppCompatActivity {
 
             roundedImageView.setImageBitmap(getGroupImage(reseiverGroup.getImage()));
             textViewImage.setText(null);
+        }
+
+
+
+        if(!preferencesManager.getString(Constants.KEY_USER_ID).equals(reseiverGroup.getUser_create())){
+
+            btnAddMember.setVisibility(View.GONE);
+            btnDeleteGroup.setVisibility(View.GONE);
+            imageViewEdit.setVisibility(View.GONE);
+
+        }else{
+            btnLeaveGroup.setVisibility(View.GONE);
         }
     }
 
@@ -248,6 +286,146 @@ public class InfoGroupActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+
+
+    }
+
+    private void dialogEditGroup(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(InfoGroupActivity.this);
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_edit_group, null);
+
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+            roundedImageViewDialog = (RoundedImageView) view.findViewById(R.id.imageGroupEdit);
+            btnSaveGroupDialog = (Button) view.findViewById(R.id.btnSaveGroupEdit);
+
+            textImageDialog =(TextView) view.findViewById(R.id.textAddImageEdit);
+
+            textInputEditTextTitle = (TextInputEditText) view.findViewById(R.id.txtTitleGroupEdit);
+            textInputEditTextDescription = (TextInputEditText) view.findViewById(R.id.txtDescriptionGroupEdit);
+
+
+            textInputEditTextTitle.setText(reseiverGroup.getTitle());
+            textInputEditTextDescription.setText(reseiverGroup.getDescription());
+            roundedImageViewDialog.setImageBitmap(getGroupImage(reseiverGroup.getImage()));
+
+
+
+            roundedImageViewDialog.setOnClickListener(v -> {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
+
+                pickImageDialog.launch(intent);
+            });
+
+            btnSaveGroupDialog.setOnClickListener(v -> {
+                if(isValidGroup()){
+
+                    saveGroup(dialog);
+
+                }
+            });
+
+
+
+
+    }
+
+    private boolean isValidGroup(){
+
+        if (textInputEditTextTitle.getText().toString().trim().isEmpty()){
+            ResourceUtil.showAlert("Advertencia", "Por favor escriba un titulo", InfoGroupActivity.this, "error");
+            return false;
+        }else if (textInputEditTextDescription.getText().toString().trim().isEmpty()){
+            ResourceUtil.showAlert("Advertencia", "Por favor escriba una descripcion", InfoGroupActivity.this, "error");
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    private void saveGroup(AlertDialog dialog){
+
+        pDialog.show();
+
+        Group group = new Group();
+
+        encodedImage = (encodedImage.isEmpty())?reseiverGroup.getImage():encodedImage;
+
+        group.setTitle(textInputEditTextTitle.getText().toString());
+        group.setDescription(textInputEditTextDescription.getText().toString());
+        group.setUser_create(reseiverGroup.getUser_create());
+        group.setImage(encodedImage);
+        group.setStatus(Group.STATUS_ACTIVE);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("id", reseiverGroup.getId());
+        params.put("idFirebase", ResourceUtil.createCodeRandom(6));
+        params.put("title", group.getTitle());
+        params.put("description", group.getDescription());
+        params.put("image", group.getImage());
+        params.put("status", group.getStatus());
+        params.put("user_id_created", group.getUser_create());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, GroupApiMethods.POST_GROUP+reseiverGroup.getId(), new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                pDialog.dismiss();
+
+                try {
+                    String resposeData = response.getString("data");
+
+
+                    if(!resposeData.equals("[]")){
+
+                        dialog.dismiss();
+
+                        textViewTitle.setText(group.getTitle());
+                        textViewDescription.setText(group.getDescription());
+
+                        if(!group.getImage().isEmpty()){
+
+                            roundedImageView.setImageBitmap(getGroupImage(group.getImage()));
+//                            textViewImage.setText(null);
+                        }
+
+
+                    }else {
+                        ResourceUtil.showAlert("Advertencia", "Se produjo un error al editar el grupo.",InfoGroupActivity.this, "error");
+                    }
+
+                } catch (JSONException e) {
+                    ResourceUtil.showAlert("Advertencia", "Se produjo un error al editar el grupo",InfoGroupActivity.this, "error");
+                }
+
+
+//                Toast.makeText(CreateGroupActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pDialog.dismiss();
+                ResourceUtil.showAlert("Advertencia", "Se produjo un error al registrar el grupo.",InfoGroupActivity.this, "error");
+                Log.d("ERROR_USER", "Error Register: "+error.getMessage());
+
+//                Toast.makeText(CreateGroupActivity.this, "Error: " +error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
 
 
     }
@@ -468,6 +646,43 @@ public class InfoGroupActivity extends AppCompatActivity {
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 
+    private final ActivityResultLauncher<Intent> pickImageDialog = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK){
+
+                    if(result.getData() != null){
+                        Uri imageUri = result.getData().getData();
+
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            roundedImageViewDialog.setImageBitmap(bitmap);
+
+//                            textImageDialog.setVisibility(View.INVISIBLE);
+
+                            encodedImage = encodeImage(bitmap);
+                        }catch (FileNotFoundException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
+    private String encodeImage(Bitmap bitmap){
+
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 60, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+
+    }
 
 
 }
