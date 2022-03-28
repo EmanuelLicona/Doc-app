69a7e5da324dad4e84f79ca1fed39cf413f23815
ang <ag888927@gmail.com>
2022-03-28 10:50:36 -0600

    Interfaz de notas

--- app/src/main/java/com/application/pm1_proyecto_final/Fragments/FrangmentApuntes.java
+++ app/src/main/java/com/application/pm1_proyecto_final/Fragments/FrangmentApuntes.java
@@ -1,24 +1,47 @@
 package com.application.pm1_proyecto_final.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.application.pm1_proyecto_final.R;
+import com.application.pm1_proyecto_final.activities.CreateGroupActivity;
+import com.application.pm1_proyecto_final.activities.NotesActivity;
+import com.application.pm1_proyecto_final.utils.PreferencesManager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class FrangmentApuntes extends Fragment {
 
-
+    FloatingActionButton buttonaddNote;
+    PreferencesManager preferencesManager;
+    RecyclerView recyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_apuntes, container, false);
+
+        preferencesManager = new PreferencesManager(getContext());
+
+        buttonaddNote = (FloatingActionButton) view.findViewById(R.id.btnaddNote);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.groupsRecyclerView);
+
+        buttonaddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), NotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/application/pm1_proyecto_final/activities/NotesActivity.java
+++ app/src/main/java/com/application/pm1_proyecto_final/activities/NotesActivity.java
@@ -1,16 +1,70 @@
 package com.application.pm1_proyecto_final.activities;
 
+import static java.security.AccessController.getContext;
+
+import androidx.activity.OnBackPressedDispatcherOwner;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.application.pm1_proyecto_final.R;
+import com.application.pm1_proyecto_final.adapters.GroupAdapter;
+import com.application.pm1_proyecto_final.utils.PreferencesManager;
+import com.application.pm1_proyecto_final.utils.ResourceUtil;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NotesActivity extends AppCompatActivity {
-
+    AppCompatImageView imageViewBack;
+    EditText txtTitle,txtDescription;
+    Button btnSaveNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        imageViewBack = (AppCompatImageView) findViewById(R.id.btnNoteBack);
+        txtTitle = (EditText) findViewById(R.id.txtTitleNote);
+        txtDescription = (EditText) findViewById(R.id.txtDescriptionNote);
+
+        btnSaveNote = (Button) findViewById(R.id.btnSaveNote);
+
+        setListener();
+
+    }
+
+    private void setListener(){
+        imageViewBack.setOnClickListener(v -> onBackPressed());
+
+        btnSaveNote.setOnClickListener(v -> {
+            if(validNote()){
+
+                saveNote();
+
+            }
+        });
+    }
+
+
+    private void saveNote(){
+
+    }
+
+
+    private boolean validNote(){
+
+        if (txtTitle.getText().toString().trim().isEmpty()){
+            ResourceUtil.showAlert("Advertencia", "Por favor escriba un titulo", NotesActivity.this, "error");
+            return false;
+        }else if (txtDescription.getText().toString().trim().isEmpty()){
+            ResourceUtil.showAlert("Advertencia", "Por favor escriba una descripcion", NotesActivity.this, "error");
+            return false;
+        }else{
+            return true;
+        }
+
     }
 }
