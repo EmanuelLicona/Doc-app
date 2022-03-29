349b89d3b9905c4ba4daa07c2274955c73beb2fb
Smolder98 <alicoescobar@gmail.com>
2022-03-28 23:27:04 -0600

    Agregando splashScreen

--- app/src/main/java/com/application/pm1_proyecto_final/activities/MainActivity.java
+++ app/src/main/java/com/application/pm1_proyecto_final/activities/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_PM1_PROYECTO_FINAL);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
